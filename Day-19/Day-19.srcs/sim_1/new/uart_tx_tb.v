cd8f3da55ba11bd6e600af5445e0aa7a8f73275b
Bharathmani <bharathmani0812005@gmail.com>
2026-03-02 21:40:27 +0530

    Day 19: UART Transmitter

---
+++ Day-19/Day-19.srcs/sim_1/new/uart_tx_tb.v
@@ -0,0 +1,42 @@
+`timescale 1ns/1ps
+
+module uart_tx_tb;
+
+    reg clk;
+    reg rst;
+    reg tx_start;
+    reg [7:0] tx_data;
+    wire tx;
+    wire tx_busy;
+
+    uart_tx #(
+        .CLK_FREQ(1000000),   // smaller for simulation
+        .BAUD_RATE(1000)
+    ) DUT (
+        .clk(clk),
+        .rst(rst),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+        .tx(tx),
+        .tx_busy(tx_busy)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        rst = 1;
+        tx_start = 0;
+        tx_data = 8'hA5;
+
+        #20 rst = 0;
+
+        #20 tx_start = 1;
+        #10 tx_start = 0;
+
+        #50000;
+
+        $stop;
+    end
+
+endmodule
